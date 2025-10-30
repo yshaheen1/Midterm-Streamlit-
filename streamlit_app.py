2bc1e800196afce4dfb3547f72bd74900d18f31b
yshaheen1 <yshaheen1@babson.edu>
2025-10-30 16:06:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,19 @@
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+last_rows = np.random.randn(1, 1)  # noqa: NPY002
+chart = st.line_chart(last_rows)
 
-def get_un_data() -> pd.DataFrame:
-    aws_bucket_url = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-    df = pd.read_csv(aws_bucket_url + "/agri.csv.gz")
-    return df.set_index("Region")
+for i in range(1, 101):
+    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)  # noqa: NPY002
+    status_text.text(f"{i}% complete")
+    chart.add_rows(new_rows)
+    progress_bar.progress(i)
+    last_rows = new_rows
+    time.sleep(0.05)
 
-try:
-    df = get_un_data()
-    countries = st.multiselect(
-        "Choose countries", list(df.index), ["China", "United States of America"]
-    )
-    if not countries:
-        st.error("Please select at least one country.")
-    else:
-        data = df.loc[countries]
-        data /= 1000000.0
-        st.subheader("Gross agricultural production ($B)")
-        st.dataframe(data.sort_index())
+progress_bar.empty()
 
-        data = data.T.reset_index()
-        data = pd.melt(data, id_vars=["index"]).rename(
-            columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-        )
-        chart = (
-            alt.Chart(data)
-            .mark_area(opacity=0.3)
-            .encode(
-                x="year:T",
-                y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                color="Region:N",
-            )
-        )
-        st.altair_chart(chart, use_container_width=True)
-except URLError as e:
-    st.error(f"This demo requires internet access. Connection error: {e.reason}")
+# Streamlit widgets automatically run the script from top to bottom. Since
+# this button is not connected to any other logic, it just causes a plain
+# rerun.
+st.button("Rerun")

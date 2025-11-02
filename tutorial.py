3db2ea393c72360fcbfb7995ef59ae7e2b61f87c
yshaheen1 <yshaheen1@babson.edu>
2025-11-02 12:56:20 -0500

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,5 +1,29 @@
+# Displaying Text and Headings
+
 import streamlit as st
-st.title("Hello Streamlit!")
-st.write("This is my first interactive dashboard.")
+
+st.title("Basic Streamlit functions")
+st.header("Section Header")
+st.subheader("Subsection Header")
+st.write("This is an interactive dashboard")
 x = st.slider("Select a value", 0, 100, 50)
 st.write("You selected:", x)
+
+# Displaying Data
+
+import pandas as pd
+
+data = pd.DataFrame({
+    "Product": ["Shoes", "Shirts", "Pants"],
+    "Sales": [100, 50, 80]
+})
+st.dataframe(data)    
+st.table(data.head())
+
+# Widgets
+name = st.text_input("Enter your name")
+age = st.slider("Select your age", 0, 100, 25)
+show = st.checkbox("Show message")
+
+if show:
+    st.write(f"Hello {name}, you are {age} years old!")
